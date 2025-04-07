6537e6c846d86e2c5b5384dd37d0040a12f1bf4e
LeonardKreil <leonard.kreil@icloud.com>
2025-04-07 15:09:43 +0200

    init file for algorithms

---
+++ bipedal_control_experiment/algorithms/__init__.py
@@ -0,0 +1,7 @@
+from algorithms.ppo import PPOTrainer
+from algorithms.sac import SACTrainer
+from algorithms.ddpg import DDPGTrainer
+from algorithms.td3 import TD3Trainer
+from algorithms.dqn import DQNTrainer
+
+__all__ = ['PPOTrainer', 'SACTrainer', 'DDPGTrainer', 'TD3Trainer', 'DQNTrainer']
